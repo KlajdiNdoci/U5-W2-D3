87ff1827cc046558df58264a1f9d867a4fe95532
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-11-08 14:18:01 +0100

    author and post association

--- src/main/java/KlajdiNdoci/U5W2D3/post/Post.java
+++ src/main/java/KlajdiNdoci/U5W2D3/post/Post.java
@@ -1,5 +1,7 @@
 package KlajdiNdoci.U5W2D3.post;
 
+import KlajdiNdoci.U5W2D3.utente.Utente;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -7,11 +9,18 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Entity
+@Table(name = "posts")
 public class Post {
+    @Id
+    @GeneratedValue
     private long id;
     private String categoria;
     private String titolo;
     private String cover;
     private String contenuto;
     private int tempoDiLettura;
+    @ManyToOne
+    @JoinColumn(name = "utente_id")
+    private Utente utente;
 }
--- src/main/java/KlajdiNdoci/U5W2D3/utente/Utente.java
+++ src/main/java/KlajdiNdoci/U5W2D3/utente/Utente.java
@@ -1,5 +1,9 @@
 package KlajdiNdoci.U5W2D3.utente;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,19 +13,15 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @ToString
+@Entity
+@Table(name = "utenti")
 public class Utente {
+    @Id
+    @GeneratedValue
     private long id;
     private String nome;
     private String cognome;
     private String email;
     private LocalDate dataDiNascita;
     private String avatar;
-
-    public Utente() {
-        updateAvatar();
-    }
-
-    private void updateAvatar() {
-        this.avatar = "https://ui-avatars.com/api/?name=" + this.nome + "+" + this.cognome;
-    }
 }
